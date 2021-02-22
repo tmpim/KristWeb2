557ae5e07c8af81db95eea8f6d1f0f55eb8eae0a
Drew Lemmy <drew@lemmmy.pw>
2021-02-22 03:44:01 +0000

    fix: whooooooops

--- src/store/actions/NodeActions.ts
+++ src/store/actions/NodeActions.ts
@@ -6,4 +6,4 @@ import * as constants from "../constants";
 export const setSyncNode = createAction(constants.SYNC_NODE)<string>();
 export const setLastBlockID = createAction(constants.LAST_BLOCK_ID)<number>();
 export const setDetailedWork = createAction(constants.DETAILED_WORK)<KristWorkDetailed>();
-export const setCurrency = createAction(constants.DETAILED_WORK)<KristCurrency>();
+export const setCurrency = createAction(constants.CURRENCY)<KristCurrency>();
