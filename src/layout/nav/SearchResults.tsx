2993ab9e5da02d125f3636d23b1f0c7c508f8115
Drew Lemmy <drew@lemmmy.pw>
2021-02-28 15:25:36 +0000

    chore: minor cleanup

--- src/layout/nav/SearchResults.tsx
+++ src/layout/nav/SearchResults.tsx
@@ -1,4 +1,7 @@
-import React, { ReactNode, FC } from "react";
+// Copyright (c) 2020-2021 Drew Lemmy
+// This file is part of KristWeb 2 under GPL-3.0.
+// Full details: https://github.com/tmpim/KristWeb2/blob/master/LICENSE.txt
+import React, { ReactNode } from "react";
 import { Typography, Spin } from "antd";
 import { LoadingOutlined } from "@ant-design/icons";
 
