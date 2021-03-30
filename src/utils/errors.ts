049889fb3aa05e09d645e514e8f4d660fe413f23
Drew Lemmy <drew@lemmmy.pw>
2021-03-30 16:45:06 +0100

    chore: remove comment

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -12,10 +12,6 @@ Sentry.init({
   dsn: "https://51a018424102449b88f94c795cf62bb7@sentry.lemmmy.pw/2",
   release: "kristweb2-react@" + gitVersion,
   integrations: [new Integrations.BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
   tracesSampleRate: 1.0
 });
 
