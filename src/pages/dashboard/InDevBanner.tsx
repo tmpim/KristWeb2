ba4d79855a5588d988b9e8f99769fd0072351d27
Drew Lemmy <drew@lemmmy.pw>
2021-06-05 15:31:45 +0100

    fix: in dev banner message

--- src/pages/dashboard/InDevBanner.tsx
+++ src/pages/dashboard/InDevBanner.tsx
@@ -15,9 +15,9 @@ export function InDevBanner(): JSX.Element {
   return <Alert
     style={{ marginBottom: 24 }}
     type="info"
-    message={<Trans t={t} i18nKey="dashboard.inDevBanner">
-      Welcome to the KristWeb v2 private beta! This site is still in development,
-      so most features are currently missing. Please report all bugs on
+    message={<Trans t={t} i18nKey="dashboard.inDevBanner2">
+      Welcome to the KristWeb v2 public beta! This site is relatively new, so
+      please report any bugs on
       <a href={gitURL + "/issues/new"} target="_blank" rel="noopener noreferrer">GitHub</a>.
       Thanks!
     </Trans>}
