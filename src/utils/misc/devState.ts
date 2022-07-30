ab2667b592dd3eb0e72ce11d7614cd04b97be848
Drew Edwards <github@drew.contact>
2022-07-30 13:02:38 +0100

    chore: register turkish localisation

--- src/utils/misc/devState.ts
+++ src/utils/misc/devState.ts
@@ -1,5 +1,6 @@
-
-
+// Copyright (c) 2020-2021 Drew Lemmy
+// This file is part of KristWeb 2 under AGPL-3.0.
+// Full details: https://github.com/tmpim/KristWeb2/blob/master/LICENSE.txt
 declare const __GIT_VERSION__: string;
 
 const devEnvs = ["development", "local", "test"];
